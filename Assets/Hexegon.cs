88e055faa9ad579e117249297e24c4d8c2c70c69
ArjunMahesh97 <arjunmahesh97@gmail.com>
2019-03-25 22:37:58 +0530

    Random Idea

---
+++ Assets/Hexegon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hexegon : MonoBehaviour
+{
+    public Rigidbody2D rigidBody;
+
+    public float shrinkSpeed = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody.rotation = Random.Range(0, 360f);
+        transform.localScale = Vector3.one * 10f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale -= Vector3.one * shrinkSpeed * Time.deltaTime;
+
+        if (transform.localScale.x <= 0.05f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public float spawnRate = 1f;
+    public GameObject hexegonPrefab;
+    private float nextSpawn = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= nextSpawn)
+        {
+            Instantiate(hexegonPrefab, Vector3.zero, Quaternion.identity);
+            nextSpawn = Time.time + 1f / spawnRate;
+        }
+    }
+}
